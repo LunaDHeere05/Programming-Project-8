8d4e524655c6c4570103177b3b5efa7a5110b06e
Luna D'Heere <161327120+LunaDHeere05@users.noreply.github.com>
2024-05-07 15:13:50 +0200

    top_nav geupdate

--- php/top_nav.php
+++ php/top_nav.php
@@ -1,6 +1,13 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-               
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+
+}           
 nav{
     display: flex;
     justify-content: space-between;
@@ -56,7 +63,7 @@ nav ul li a{
 .zoekbalk_container{
     <?php
         echo 'background: url(images\jpg\jj-ying-7JX0-bfiuxQ-unsplash.jpg) no-repeat center center/cover;';
-    ?>
+    ?>;
     height: 6em;
     display: flex;
 }
