500a77b732f9e5d3138d080cdd25e98b34fefd38
Luna D'Heere <161327120+LunaDHeere05@users.noreply.github.com>
2024-05-07 11:15:22 +0200

    php files in 1 map gestoken

---
+++ php/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "dt5.ehb.be"; // Change this to your database server
+$username = "2324PROGPROJGR8"; // Change this to your database username
+$password = "P!j6WD5KL"; // Change this to your database password
+$database = "2324PROGPROJGR8"; // Change this to your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
