9d9eea6b4c72427eea3a9af74052a1785a5241ab
anissahli <anissa.sahli@student.ehb.be>
2024-05-15 22:09:56 +0200

    funcitonaliteit voor beschikbaarheid van een item; kijken of een exemplaar van het item beschikbaar is - indien niet, welk het vroegst weer uitleenbaar is.

--- php/Inventaris.php
+++ php/Inventaris.php
@@ -136,17 +136,27 @@
   border: none;
 }
 
+.beschikbaar{
+  color:#1BBCB6;
+}
+
 .beschikbaarheid_apparaat {
   display: flex;
-  width: 20%;
-  color: #1bbcb6;
+  flex-direction: column;
+  align-items: center;
+  width: 30%;
+  color: #E30613;
   font-weight: bold;
 }
+
+.beschikbaarheid_apparaat p{
+  font-weight:lighter;
+
+}
 .beschikbaarheid_apparaat img {
-  width: 4em;
-  margin: 0em 2em;
-  filter: invert(62%) sepia(49%) saturate(680%) hue-rotate(129deg)
-    brightness(90%) contrast(89%);
+  width: 3em;
+  margin: 0.5em 2em;
+
 }</style>
 
 <?php include("footer.php"); ?>
