a42988e081721a6b5711eea44eb9d6ce851d6cfb
lord-nox <lars.paridaens@student.ehb.be>
2024-05-16 15:46:50 +0200

    Update dbh_pdo.php

--- dbh_pdo.php
+++ dbh_pdo.php
@@ -8,11 +8,11 @@ $password = 'P!j6WD5KL'; // Database password
 try {
     // Connect to the database using PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // Set PDO error mode to exception
+    // Set PDO error mode  to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Optionally, set other PDO attributes here
 } catch (PDOException $e) {
     // If connection fails, display an error message
-    die("Failed to connect to the database: " . $e->getMessage());
+    die("Failed to connect to the getMessagedatabase: " . $e->());
 }
 ?>
