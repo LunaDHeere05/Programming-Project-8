c81cbeff834e837a479b05733f052495921c1431
edsondes <edsondes@icloud.com>
2024-05-20 00:23:01 +0200

    uitleningen kunnen toegevoegd worden, maar de uitgeleende items moeten nog aan de uitlening verbonden worden

--- admin_php/functies/uitlening_toevoegen.php
+++ admin_php/functies/uitlening_toevoegen.php
@@ -15,25 +15,60 @@ if ($conn->connect_error) {
 
     if (isset($_POST['bevestig_button'])) {
         $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-        $email = filter_input(INPUT_POST, "apparaat", FILTER_SANITIZE_SPECIAL_CHARS);
-        $datum = $_POST['datum'];
+        $apparaat = filter_input(INPUT_POST, "apparaat", FILTER_SANITIZE_SPECIAL_CHARS);
+        $inDatum = $_POST['datum'];
 
         // Query om te checken of de email bestaat in de database
         $emailquery = "SELECT email FROM STUDENT WHERE email = '$email'
         UNION 
         SELECT email FROM DOCENT WHERE email = '$email'";
         // Query om te checken of het apparaat bestaat in de database
-        $apparaatquery = "SELECT naam FROM ITEM WHERE naam = '$apparaat'";
+        $apparaatquery = "SELECT naam, item_id FROM ITEM WHERE naam = '$apparaat'";
+        // variabele voor de huidige datum
+        $uitDatum = date("Y-m-d");
 
+     
         // check om te kijken of de velden zijn ingevuld
-        if(empty($email||$apparaat||$datum)){
+        if(empty($email||$apparaat||$inDatum)){
             echo "vul alle velden in!";
         } else {
             $emailResult = mysqli_query($conn, $emailquery);
             if ($emailResult){
                 $apparaatresult = mysqli_query($conn, $apparaatquery);
                 if($apparaatresult){
+                    $email_row = mysqli_fetch_assoc($emailResult);
+                    $apparaat_row = mysqli_fetch_assoc($apparaatresult);
+                    // echo "$email_row[email] <br>";
+                    // echo "$apparaat_row[naam] <br>";    
+                    
 
+                    // Query om de uitlening toe te voegen aan de database
+                    // eerst wordt er een nieuwe uitlening toegevoegd
+                    
+                    // dan voeg ik een nieuw uitgeleend_item toe aan de uitlening(via uitleen_id)
+                    if(isset($_POST['email_type'])){
+                        $email_type = $_POST['email_type'];
+                        if($email_type == "student"){
+                           $sql_lening = "INSERT INTO `UITLENING` (`uitleen_id`, `uitleen_datum`, `inlever_datum`, `isOpgehaald`, `isVerlengd`, `emailSTUDENT`, `emailDOCENT`) VALUES (NULL, '$uitDatum', '$inDatum', '0', '0', '$email',  NULL)" ;
+                           $stmt = $conn->prepare($sql_lening);
+                            if($stmt->execute()){
+                                echo "Uitlening toegevoegd!";
+                            } else {
+                                echo "Uitlening niet toegevoegd!";
+                            }
+                        } else {
+                           $sql_lening= "INSERT INTO `UITLENING` (`uitleen_id`, `uitleen_datum`, `inlever_datum`, `isOpgehaald`, `isVerlengd`, `emailSTUDENT`, `emailDOCENT`) VALUES (NULL, '$uitDatum', '$inDatum', '0', '0', NULL, '$email')"; 
+                            $stmt = $conn->prepare($sql_lening); 
+                            if($stmt->execute()){
+                                echo "Uitlening toegevoegd!";
+                            } else {
+                                echo "Uitlening niet toegevoegd!";
+                            }
+                        }
+                    } 
+            
+                } else {
+                    echo "Apparaat niet gevonden!";
                 }
             } else {
                 echo "Email niet gevonden!";
