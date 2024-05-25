33b56d2b0501f0039b8343ffca2599ffb8992fbd
dominium2 <danial.mubeasher@student.ehb.be>
2024-05-25 15:33:02 +0200

    apparaat toevoegen confiramayie werkt

--- admin_php/InventarisToevoegen.php
+++ admin_php/InventarisToevoegen.php
@@ -6,7 +6,7 @@
   <title>Apparaat toevoegen</title>
   <?php include 'top_nav_admin.php'?>
   <style>
-    .inventaris_toe_specificaties {
+    .inventaris_toe_specificaties { 
       margin: 1em;
       background-color: #D9D9D9;
       border-radius: 2em;
@@ -90,28 +90,36 @@
       margin: 0em 0em 0em 0.5em;
       filter: invert(100%) sepia(0%) saturate(5878%) hue-rotate(26deg) brightness(116%) contrast(115%);
     } 
+
+    /* The Modal (background) */
+    .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 1; /* Sit on top */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+      background-color: #fefefe;
+      margin: 15% auto; /* 15% from the top and centered */
+      padding: 20px;
+      border: 1px solid #888;
+      width: 80%; /* Could be more or less, depending on screen size */
+    }
   </style>
 </head>
 <body>
-
-<script>
-  function addInputField() {
-      // Create a new input element
-      var newInput = document.createElement("input");
-
-      // Set the input's attributes
-      newInput.setAttribute("name", "functionaliteit[]");
-      newInput.setAttribute("type", "text");
-      newInput.setAttribute("placeholder", "Apparaat beschrijving ...");
-
-      // Append the new input to the container
-      document.querySelector(".inventaris_toe_text").appendChild(newInput);
-  }
-</script>
   <div class="inventaris_toe_specificaties">
     <div class="inventaris_toe_block">
       <div class="inventaris_toe_block1"> 
-        <form action="functies/InventarisToevoegenFunctie.php" method="post" enctype="multipart/form-data">
+        <form id="form" action="functies/InventarisToevoegenFunctie.php" method="post" enctype="multipart/form-data">
           <div class="inventaris_toe">
             <h2>Apparaat naam:</h2>
             <input id="apparaat_naam" name="apparaat_naam" type="text">
@@ -128,7 +136,6 @@
             <h2>Beschrijving:</h2>
             <input id="beschrijving" name="beschrijving" type="text">
           </div>
-          
           <input type="file" name="image">
           <input type="text" name="link" placeholder="Link naar handleiding">
           <div class="inventaris_toe_text">
@@ -137,17 +144,72 @@
           <button type="button" onclick="addInputField()">Add another field</button>
           <div class="inventaris_toe_buttons">
             <div class="inventaris_toe_opslaan">
-              <button name="submit" type="submit">upload</button>
+              <button name="submitForm" type="button">Bevestigen</button>
+              <input type="hidden" id="add_item" name="submitForm" value="save">
             </div>
           </div>
         </form>
-        
       </div>
       <div class="inventaris_toe_img">
-        
       </div>
     </div>
-    
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <p>Are you sure you want to submit?</p>
+        <button id="yesBtn">Yes</button>
+        <button id="noBtn">No</button>
+      </div>
+    </div>
   </div>
 </body>
 </html>
+
+<script>
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.querySelector(".inventaris_toe_opslaan button");
+
+  // Get the <span> element that closes the modal
+  var yesBtn = document.getElementById("yesBtn");
+  var noBtn = document.getElementById("noBtn");
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on yes
+  yesBtn.onclick = function() {
+    modal.style.display = "none"; // Hide the modal
+    form.submit(); // Submit the form
+  }
+
+  // When the user clicks on no
+  noBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  function addInputField() {
+    // Create a new input element
+    var newInput = document.createElement("input");
+
+    // Set the input's attributes
+    newInput.setAttribute("name", "functionaliteit[]");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("placeholder", "Apparaat beschrijving ...");
+
+    // Append the new input to the container
+    document.querySelector(".inventaris_toe_text").appendChild(newInput);
+  }
+</script>
--- admin_php/functies/InventarisToevoegenFunctie.php
+++ admin_php/functies/InventarisToevoegenFunctie.php
@@ -2,10 +2,8 @@
 
 include '../database.php';
 
-
-
 // Check if the form is submitted
-if (isset($_POST['submit'])) {
+if (isset($_POST['submitForm'])) {
     $apparaat = $_POST['apparaat_naam'];
     $merk = $_POST['merk'];
     $categorie = $_POST['categorie'];
@@ -15,7 +13,7 @@ if (isset($_POST['submit'])) {
     $functionaliteit = $_POST['functionaliteit'];
 
     // Check if there is already a row with the same apparaat_naam
-    $checkQuery = "SELECT * FROM ITEM WHERE naam = '$apparaat'AND merk = '$merk'";
+    $checkQuery = "SELECT * FROM ITEM WHERE naam = '$apparaat' AND merk = '$merk'";
     $checkResult = $conn->query($checkQuery);
 
     if ($checkResult->num_rows > 0) {
@@ -34,8 +32,8 @@ if (isset($_POST['submit'])) {
         $itemQuery = "INSERT INTO ITEM (naam, merk, categorie, beschrijving, gebruiksaanwijzing, image_id) VALUES ('$apparaat','$merk','$categorie','$beschrijving', '$link', '$image_id')";
         if ($conn->query($itemQuery) === TRUE) {
             $item_id = $conn->insert_id;
-            
-             // Get the auto-generated item_id from the newly created row
+
+            // Get the auto-generated item_id from the newly created row
             $exemplaarItemQuery = "INSERT INTO EXEMPLAAR_ITEM (item_id) VALUES ('$item_id')";
             $conn->query($exemplaarItemQuery);
 
