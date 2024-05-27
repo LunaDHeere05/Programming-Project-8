dcbf2f5ab9a0014a2df5c966d14639b238bb8781
lord-nox <lars.paridaens@student.ehb.be>
2024-05-27 11:34:44 +0200

    dashboard, laten ophalen en inleveren van Items , ook per apparaat nu zichtbaar ipv per reservatie

--- admin_php/functies/verwijder_reservatie.php
+++ admin_php/functies/verwijder_reservatie.php
@@ -2,20 +2,36 @@
 // verwijder_reservatie.php
 
 // Inclusie van databaseverbinding
-include 'database.php';
+include '../database.php';
 
 // Ontvang de reservatie-ID van de AJAX-verzoek
 $reservatieID = $_POST['reservatieID'];
 
+echo "checkpoint 1";
+mysqli_begin_transaction($conn);
 
-// Query om de reservatie uit de database te verwijderen
-$query = "DELETE FROM UITLENING WHERE uitleen_id = '$reservatieID'";
+try {
+    // Delete related records from UITGELEEND_ITEM first
+    $query1 = "DELETE FROM UITGELEEND_ITEM WHERE uitleen_id = '$reservatieID'";
+    if (!mysqli_query($conn, $query1)) {
+        throw new Exception("Error deleting from UITGELEEND_ITEM: " . mysqli_error($conn));
+    }
 
-// Uitvoeren van de query
-if (mysqli_query($conn, $query)) {
+    // Delete the record from UITLENING
+    $query2 = "DELETE FROM UITLENING WHERE uitleen_id = '$reservatieID'";
+    if (!mysqli_query($conn, $query2)) {
+        throw new Exception("Error deleting from UITLENING: " . mysqli_error($conn));
+    }
+
+    // Commit the transaction
+    mysqli_commit($conn);
+
     echo "Reservatie succesvol verwijderd.";
-} else {
-    echo "Er is een fout opgetreden bij het verwijderen van de reservatie: " . mysqli_error($conn);
+} catch (Exception $e) {
+    // Rollback the transaction in case of error
+    mysqli_rollback($conn);
+
+    echo "Er is een fout opgetreden bij het verwijderen van de reservatie: " . $e->getMessage();
 }
 
 // Sluit de databaseverbinding
