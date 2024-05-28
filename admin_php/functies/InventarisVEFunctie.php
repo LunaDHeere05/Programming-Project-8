e429cad74163cd85c5a65627cdc4e9c32e947d18
dominium2 <danial.mubeasher@student.ehb.be>
2024-05-28 10:57:25 +0200

    item deleten wanneer er geen exemplaar is
    wanneer er geen item meer zijn word het item verwijdert

--- admin_php/functies/InventarisVEFunctie.php
+++ admin_php/functies/InventarisVEFunctie.php
@@ -9,9 +9,44 @@ if (isset($_GET['exemplaar_item_id']))
     $item_id = $_GET['item_id'];
     $deleteExemplaarItemQuery = "DELETE FROM EXEMPLAAR_ITEM WHERE exemplaar_item_id='$exemplaar_item_id'";
     if ($conn->query($deleteExemplaarItemQuery) === TRUE) {
-        echo "Record deleted successfully";
+        echo "Record exemplaar deleted successfully";
     } else {
-        echo "Error deleting record: " . $conn->error;
+        echo "Error deleting exemplaar record: " . $conn->error;
     }
+
+    
+
+    // Check if there are any rows with the same item_id in EXEMPLAAR_ITEM
+    $checkExemplaarItemQuery = "SELECT * FROM EXEMPLAAR_ITEM WHERE item_id='$item_id'";
+    $result = $conn->query($checkExemplaarItemQuery);
+    if ($result->num_rows == 0) {
+        // Delete FUNCTIONALITEIT rows
+        $deleteFunctionaliteitQuery = "DELETE FROM FUNCTIONALITEIT WHERE item_id='$item_id'";
+        if ($conn->query($deleteFunctionaliteitQuery) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . $conn->error;
+        }
+
+        // Delete Recent item row
+        $deleteRecentItemQuery = "DELETE FROM RECENT_ITEMS WHERE item_id='$item_id'";
+        if ($conn->query($deleteRecentItemQuery) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . $conn->error;
+        }
+
+        // Delete ITEM row
+        $deleteItemQuery = "DELETE FROM ITEM WHERE item_id='$item_id'";
+        if ($conn->query($deleteItemQuery) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . $conn->error;
+        }
+        $conn->close();
+        header('Location: ../Inventaris.php');
+    }
+
+    $conn->close();
     header("Location: ../InventarisExemplaars.php?item_id=$item_id");
 }
