fa4446ab7c8ccb3834acc046ff5b5dfc8c7f8317
Amina-Iqbal <145972513+Amina-Iqbal@users.noreply.github.com>
2024-05-29 14:38:09 +0200

    kreeg maar 1 resultaat while loop bijgevoegd en nog fouten opgelost

--- admin_php/functies/uitleningen_zoeken.php
+++ admin_php/functies/uitleningen_zoeken.php
@@ -10,33 +10,36 @@ if (isset($_GET['zoekButton'])) {
  JOIN UITGELEEND_ITEM ui ON u.uitleen_id = ui.uitleen_id
  JOIN EXEMPLAAR_ITEM ei ON ui.exemplaar_item_id = ei.exemplaar_item_id
  JOIN ITEM i ON ei.item_id = i.item_id
- WHERE u.email LIKE '%$zoek_query%' OR ei.exemplaar_item_id LIKE '%$zoek_query%'";
+ WHERE u.email LIKE '%$zoek_query%' OR ei.exemplaar_item_id LIKE '%$zoek_query%' OR i.merk LIKE '%$zoek_query%' OR i.naam LIKE '%$zoek_query%'";
         $zoek_uitvoering_resultaat = mysqli_query($conn, $zoek_resultaat);
 
         if ($zoek_uitvoering_resultaat) {
-            if (mysqli_num_rows($zoek_uitvoering_resultaat) > 0) {
-                $result = mysqli_fetch_assoc($zoek_uitvoering_resultaat);
-                echo "<div class='result_tabel'>";
-                echo "<table>";
-                echo " <th>E-mail</th>";
-                echo "<th>Apparaat</th>";
-                echo " <th>Inleverdatum</th>";
-                echo " <th>Meer info</th>";
-                echo " <th>Verwijder</th>";
-                echo "<tr>";
-                echo "<td>" . htmlspecialchars($result['email']) . "</td>";
-                echo "<td>" . htmlspecialchars($result['exemplaar_item_id']) . "</td>";
-                echo "<td>" . htmlspecialchars($result['inlever_datum']) . "</td>";
-                echo "<td><a href='#'><img class='meer_info' src='images/svg/circle-info-solid.svg' alt='meer informatie'></a></td>";
-                echo "<td><a href='#'><img class='verwijder' src='images/svg/circle-xmark-solid.svg' alt='verwijder van blacklist'></a></td>";
-                echo "</tr>";
-                echo "</table>";
-                echo "</div>";
-                echo "<style>";
-                echo ".uitlening_tabel{
-                    display: none;}";
-                echo "</style>";
-            } else {
+          if (mysqli_num_rows($zoek_uitvoering_resultaat) > 0) {
+              echo "<div class='result_tabel'>";
+              echo "<table>";
+              echo "<tr>";
+              echo " <th>E-mail</th>";
+              echo " <th>Apparaat</th>";
+              echo " <th>Inleverdatum</th>";
+              echo " <th>Meer info</th>";
+              echo " <th>Verwijder</th>";
+              echo "</tr>";
+
+              while ($result = mysqli_fetch_assoc($zoek_uitvoering_resultaat)) {
+                  echo "<tr>";
+                  echo "<td>" . htmlspecialchars($result['email']) . "</td>";
+                  echo "<td>" . htmlspecialchars($result['merk'] . ' - ' . $result['naam']) . "</td>";
+                  echo "<td>" . htmlspecialchars($result['inlever_datum']) . "</td>";
+                  echo "<td><a href='#'><img class='meer_info' src='images/svg/circle-info-solid.svg' alt='meer informatie'></a></td>";
+                  echo "<td><a href='#'><img class='verwijder' src='images/svg/circle-xmark-solid.svg' alt='verwijder van blacklist'></a></td>";
+                  echo "</tr>";
+              }
+              echo "</table>";
+              echo "</div>";
+              echo "<style>";
+              echo ".uitlening_tabel { display: none; }";
+              echo "</style>";
+          } else {
                 echo "Geen resultaten gevonden";
             }
         } else {
